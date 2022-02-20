209ab3f4987c6f8f52d5969fc8e5fa883b3b6761
ohishim <m.ohishi.stat@gmail.com>
2022-02-20 17:21:26 +0900

    Initial commit

---
+++ R/adj.R
@@ -0,0 +1,11 @@
+#' An adjacent information for example dataset.
+#'
+#' A dataset is the adjacent information for spatio-temporal in example dataset.
+#'
+#' @format A data frame with 1180 rows and 2 variables:
+#' \describe{
+#'   \item{lab}{labels for spatio-temporal}
+#'   \item{adj}{adjacent labels}
+#' }
+#' @source none
+"adj"
---
+++ R/exdata.R
@@ -0,0 +1,14 @@
+#' An example dataset.
+#'
+#' A dataset is spatio-temporal data for 10 areas and 20 time points.
+#'
+#' @format A data frame with 200 rows and 5 variables:
+#' \describe{
+#'   \item{y}{response variables according to binomial distribution}
+#'   \item{m}{the numbers of trials}
+#'   \item{area}{locations}
+#'   \item{year}{time points}
+#'   \item{lab}{labels for spatio-temporal}
+#' }
+#' @source none
+"exdata"
