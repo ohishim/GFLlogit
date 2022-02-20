293fdbbf0716274c8aa9713e626d660af9c270c3
ohishim <m.ohishi.stat@gmail.com>
2022-02-20 16:44:24 +0900

    Update

--- R/exdata0.R
+++
@@ -1,9 +0,0 @@
-
-exdata <- read.table("data0/exdata.txt", header = T)
-adj <- read.table("data0/adj.txt", header = T)
-
-# devtools::use_data(exdata)
-# devtools::use_data(adj)
-
-# save(exdata, file="data/exdata.rda")
-# save(adj, file="data/adj.rda")
