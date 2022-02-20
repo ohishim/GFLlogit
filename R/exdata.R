9e061a55d5d4e70beb86912bed0f1f2c347a6d80
ohishim <m.ohishi.stat@gmail.com>
2022-02-20 16:34:49 +0900

    Initial commit

--- R/exdata.R
+++ R/exdata.R
@@ -1,3 +1,9 @@
 
 exdata <- read.table("data/exdata.txt", header = T)
 adj <- read.table("data/adj.txt", header = T)
+
+# devtools::use_data(exdata)
+# devtools::use_data(adj)
+
+save(exdata, file="data/exdata.rda")
+save(adj, file="data/adj.rda")
