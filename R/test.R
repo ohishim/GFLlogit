a6dc12be2c2149b4b47358593698f779cbb4b869
ohishim <m.ohishi.stat@gmail.com>
2022-02-21 08:54:54 +0900

    test commit

---
+++ R/test.R
@@ -0,0 +1,12 @@
+#' @title test
+#' @description \code{test} This is test function.
+#'
+#' @param n the number
+#' @return testobject
+#' @export
+#' @examples
+#' #test(n)
+
+test <- function(n){
+  return(rep(testobject, n))
+}
